f9241b9c2804250d984a82c0bc7c28e62bf0b25f
jan438 <jan438@users.noreply.github.com>
2025-12-22 09:30:45 +0100

    2e rect added

--- Certificate.py
+++ Certificate.py
@@ -3,11 +3,17 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib.colors import red, blue, yellow, green
 from reportlab.lib.units import mm, cm
 
-c = Canvas("PDF/Certificate.pdf", pagesize=letter)  
+c = Canvas("PDF/Certificate.pdf", pagesize=letter)
+c.saveState()
 p = c.beginPath()
 p.rect(0, 0, 5*cm, 5*cm)
 c.clipPath(p, stroke=0)
 c.linearGradient(0, 0, 5*cm, 5*cm , (red, green, yellow), (0, 0.3, 1))
+c.restoreState()
+p = c.beginPath()
+p.rect(10*cm, 10*cm, 15*cm, 15*cm)
+c.clipPath(p, stroke=0)
+c.linearGradient(10*cm, 10*cm, 15*cm, 15*cm , (red, green, yellow), (0, 0.3, 1))
 c.save()  
 
 key = input("Wait")
