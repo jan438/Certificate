2f6a83284cddd159bb4a2cbc2b3be547088b1911
jan438 <jan438@users.noreply.github.com>
2025-12-22 09:57:52 +0100

    platform added

--- Certificate.py
+++ Certificate.py
@@ -1,8 +1,15 @@
+import os
+import sys
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib.pagesizes import letter  
 from reportlab.lib.colors import red, blue, yellow, green
 from reportlab.lib.units import mm, cm
 
+if sys.platform[0] == 'l':
+    path = '/home/jan/git/Certificate'
+if sys.platform[0] == 'w':
+    path = "C:/Users/janbo/OneDrive/Documents/GitHub/Certificate"
+os.chdir(path)
 c = Canvas("PDF/Certificate.pdf", pagesize=letter)
 c.saveState()
 p = c.beginPath()
