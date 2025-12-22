0aef83fd6203c2db87e93a6ac9e5078041da0b1d
jan438 <jan438@users.noreply.github.com>
2025-12-22 11:22:37 +0100

    drawrect added

--- Certificate.py
+++ Certificate.py
@@ -1,10 +1,24 @@
 import os
 import sys
 from reportlab.pdfgen.canvas import Canvas
-from reportlab.lib.pagesizes import letter  
-from reportlab.lib.colors import red, blue, yellow, green
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.colors import red, blue, yellow, green, HexColor
 from reportlab.lib.units import mm, cm
 
+def drawRect(c, x, y, w, h, a, color):    
+    c.setFillColor(HexColor(color))
+    p = c.beginPath()
+    p.moveTo(x, y + 0.5 * a)
+    p.arcTo(x, y, x + a, y + a, startAng = 180, extent = 90)  # arc left below
+    p.lineTo(x + w, y)                                                           # horizontal line
+    p.arcTo(x + w, y, x + w + a, y + a, startAng = 270, extent = 90)  # arc right below
+    p.lineTo(x + w + a, y + h)                                                      # vertcal line
+    p.arcTo(x + w, y + h, x + w + a, y + h + a, startAng = 0, extent = 90)     # arc right above
+    p.lineTo(x + 0.5 * a, y + h + a)                                                   # horizontal line
+    p.arcTo(x, y + h, x + a, y + h + a, startAng = 90, extent = 90)    # arc left above
+    p.lineTo(x, y + 0.5 * a)                                                                # vertcal line
+    c.drawPath(p, stroke = 0, fill = 1)
+    
 if sys.platform[0] == 'l':
     path = '/home/jan/git/Certificate'
 if sys.platform[0] == 'w':
@@ -21,6 +35,7 @@ p = c.beginPath()
 p.rect(10*cm, 10*cm, 10*cm, 10*cm)
 c.clipPath(p, stroke=0)
 c.linearGradient(10*cm, 10*cm, 20*cm, 20*cm , (red, green, yellow), (0, 0.3, 1))
+drawRect(c, 10*cm,  19*cm, 100, 100, 20, "#da23ff")
 c.save()  
 
 key = input("Wait")
