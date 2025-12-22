6c7eadae73398780ea097a28c19eed68d24d70d3
jan438 <jan438@users.noreply.github.com>
2025-12-22 14:00:43 +0100

    richtin added

--- Certificate.py
+++ Certificate.py
@@ -21,6 +21,10 @@ def drawRoundedRect(c, x, y, w, h, a, d, color1, color2, color3):
     c.clipPath(p, stroke=0)
     if d == 'd':
          c.linearGradient(x, y, x + w + a, y + h + a, (color1, color2, color3), (0, 0.5, 1))
+    if d == 'h':
+         c.linearGradient(x, y, x + w + a, y, (color1, color2, color3), (0, 0.5, 1))
+    if d == 'v':
+         c.linearGradient(x, y, x, y + h + a, (color1, color2, color3), (0, 0.5, 1))
     c.restoreState()
 
 def drawRect(c, x, y, w, h, color):    
@@ -43,6 +47,8 @@ if sys.platform[0] == 'w':
 os.chdir(path)
 c = Canvas("PDF/Certificate.pdf", pagesize=letter)
 drawRoundedRect(c, 11*cm,  12*cm, 50, 50, 20, 'd', "#da23ff", "#99ff99", "#9869ff")
+drawRoundedRect(c, 11*cm,  15*cm, 50, 50, 20, 'h', "#da23ff", "#99ff99", "#9869ff")
+drawRoundedRect(c, 11*cm,  18*cm, 50, 50, 20, 'v', "#da23ff", "#99ff99", "#9869ff")
 drawRect(c, 11*cm,  6*cm, 50, 50, "#da23ff")
 c.save()  
 
