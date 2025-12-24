fe9fc546b09b6498bf90ed9f32f2ae1d9c20e40f
jan438 <jan438@users.noreply.github.com>
2025-12-24 13:57:48 +0100

    nog een step

--- Certificate.py
+++ Certificate.py
@@ -56,7 +56,7 @@ c.setFont("Helvetica-Bold", 36)
 c.setFillColor(white)  
 c.drawCentredString(width/2, height - 160, "CERTIFICATE OF ACHIEVEMENT")
 drawing = scaleSVG("certificate-award.svg", 0.23)
-renderPDF.draw(drawing, c, width - 270, 115)
+renderPDF.draw(drawing, c, width - 280, 110)
 drawSealEllipse(c)
 c.save()  
 
