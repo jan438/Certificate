fd5f168a5f5943ad445f07196a7e50fd51022f1a
jan438 <jan438@users.noreply.github.com>
2026-01-09 13:31:59 +0100

    aecersice added

---
+++ Exercise2.py
@@ -0,0 +1,17 @@
+import os
+import sys
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.colors import Color, red, blue, yellow, green, white, HexColor
+from reportlab.lib.units import mm, cm
+
+if sys.platform[0] == 'l':
+    path = '/home/jan/git/Certificate'
+if sys.platform[0] == 'w':
+    path = "C:/Users/janbo/OneDrive/Documents/GitHub/Certificate"
+os.chdir(path)
+c = Canvas("PDF/Exercise2.pdf", pagesize=letter)
+
+c.save()  
+
+key = input("Wait")
