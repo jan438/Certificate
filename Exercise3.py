c9cc86567343b20508c57aea6635a80cb417eb99
jan438 <jan438@users.noreply.github.com>
2026-02-15 14:23:17 +0100

    rir added

--- Exercise3.py
+++ Exercise3.py
@@ -5,12 +5,15 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib.colors import Color, red, blue, yellow, green, white, darkblue, gold,  HexColor
 from reportlab.lib.units import mm, cm
     
-def drawRect(c, x, y, w, h):    
+def drawRect(c, x, y, w, h, d):    
     c.saveState()
     p = c.beginPath()
     p.rect(x, y, w, h)
     c.clipPath(p, stroke = 1)
-    c.linearGradient(x, y, x, h, (darkblue, gold), (0, 1))
+    if d == "b":
+        c.linearGradient(x, y, x, h, (darkblue, gold), (0, 1))
+    if d == "t":
+        c.linearGradient(x, y, x, h, (darkblue, gold), (1, 0))
     c.restoreState()
 
 if sys.platform[0] == 'l':
@@ -19,9 +22,10 @@ if sys.platform[0] == 'w':
     path = "C:/Users/janbo/OneDrive/Documents/GitHub/Certificate"
 os.chdir(path)
 c = Canvas("PDF/Exercise3.pdf", pagesize=letter)
-drawRect(c, 100, 100, 50, 200)
-drawRect(c, 150, 100, 50, 200)
-drawRect(c, 200, 100, 50, 200)
+drawRect(c, 100, 100, 50, 200, "b")
+drawRect(c, 150, 100, 50, 200, "t")
+drawRect(c, 200, 100, 50, 200, "b")
+drawRect(c, 250, 100, 50, 200, "t")
 c.save()  
 
 key = input("Wait")
