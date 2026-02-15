d6731643174b1831ae15d92811e5b461f9e2b7a9
jan438 <jan438@users.noreply.github.com>
2026-02-15 13:43:11 +0100

    exercise added

---
+++ Exercise3.py
@@ -0,0 +1,52 @@
+import os
+import sys
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.colors import Color, red, blue, yellow, green, white, darkblue, gold,  HexColor
+from reportlab.lib.units import mm, cm
+
+def drawRoundedRect(c, x, y, w, h, a, d, color1, color2, color3):    
+    c.saveState()
+    p = c.beginPath()
+    p.moveTo(x, y + 0.5 * a)
+    p.arcTo(x, y, x + a, y + a, startAng = 180, extent = 90)                # arc left below
+    p.lineTo(x + w, y)                                                      # horizontal line
+    p.arcTo(x + w, y, x + w + a, y + a, startAng = 270, extent = 90)        # arc right below
+    p.lineTo(x + w + a, y + h)                                              # vertcal line
+    p.arcTo(x + w, y + h, x + w + a, y + h + a, startAng = 0, extent = 90)  # arc right above
+    p.lineTo(x + 0.5 * a, y + h + a)                                        # horizontal line
+    p.arcTo(x, y + h, x + a, y + h + a, startAng = 90, extent = 90)         # arc left above
+    p.lineTo(x, y + 0.5 * a)                                                # vertcal line
+    c.drawPath(p, stroke = 0, fill = 1)
+    c.clipPath(p, stroke=0)
+    if d == 'd':
+         c.linearGradient(x, y, x + w + a, y + h + a, (color1, color2, color3), (0, 0.5, 1))
+    if d == 'h':
+         c.linearGradient(x, y, x + w + a, y, (color1, color2, color3), (0, 0.5, 1))
+    if d == 'v':
+         c.linearGradient(x, y, x, y + h + a, (color1, color2, color3), (0, 0.5, 1))
+    c.restoreState()
+    
+def drawRect(c):    
+    c.saveState()
+    p = c.beginPath()
+    p.rect(100, 150, 400, 80)
+    c.clipPath(p, stroke = 0)
+    c.linearGradient(0, 0, 400, 0, (darkblue, gold), (0, 1))
+    c.restoreState()
+
+if sys.platform[0] == 'l':
+    path = '/home/jan/git/Certificate'
+if sys.platform[0] == 'w':
+    path = "C:/Users/janbo/OneDrive/Documents/GitHub/Certificate"
+os.chdir(path)
+c = Canvas("PDF/Exercise3.pdf", pagesize=letter)
+drawRect(c)
+drawRoundedRect(c, 11*cm,  15*cm, 50, 50, 20, 'h', "#da23ff", "#99ff99", "#9869ff")
+drawRoundedRect(c, 11*cm,  18*cm, 50, 50, 20, 'v', "#da23ff", "#99ff99", "#9869ff")
+red50transparent = Color( 100, 0, 0, alpha=0.5)
+c.setFillColor(red50transparent)
+c.rect(300,75,100,100, fill=True, stroke=False)
+c.save()  
+
+key = input("Wait")
